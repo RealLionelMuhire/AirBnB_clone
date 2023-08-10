2faf0be631b2a84c6f23ede509a49e15e9d06534
Geno <yvesgeno@outlook.com>
2023-08-10 22:51:19 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,20 @@ import uuid
 
 class BaseModel:
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """defining all common attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                self.id = str(uuid.uuid4())
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Helps to update update_at"""
